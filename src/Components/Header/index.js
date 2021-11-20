21e20ec60cb851c2206f5d997fdcf0847991d760
David T. John <davidjohn_atl@yahoo.com>
2021-11-20 13:39:38 -0500

    removed cra default styles and added header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <header>
+            <div></div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">Hello</a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+export default Header;
